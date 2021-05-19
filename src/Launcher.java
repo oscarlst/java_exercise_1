4a931e2039d2bf566945d53866f161c82f635c05
oscarlussot <oscar.lussot@epita.fr>
2021-05-19 12:21:21 +0200

    Partie 4 - une première commande

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,15 +4,27 @@ public class Launcher {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Welcome everybody !!!");
-        String inputUser = myObj.nextLine();
+        String input = myObj.nextLine();
 
-        while (!inputUser.equals("quit")) {
-            if (inputUser.equals("quit")) {
-                System.exit(0);
+        while (!input.equals("quit")) {
+            if (input.equals("fibo")) {
+                System.out.println("Veuillez saisir la valeur de n :");
+                input = myObj.nextLine();
+                System.out.println(fibonacci(Integer.parseInt(input)));
             } else {
                 System.out.println("Unknown command");
             }
-            inputUser = myObj.nextLine();
+            input = myObj.nextLine();
+        }
+    }
+
+    private static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 }
